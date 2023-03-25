63ade07a4ee4fc507199950c553f92175c0725c3
MariaMagallon <maria.magallon@estudiantat.upc.edu>
2023-03-26 00:46:57 +0100

    add favicon + scticky navbar

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   var reveals = document.querySelectorAll(".reveal");
+  var navbar = document.getElementById("header");
+  var sticky = navbar.offsetTop;
 
+  //reveal items effect by adding a class on scroll
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var revealTop = reveals[i].getBoundingClientRect().top;
@@ -11,4 +14,13 @@ window.addEventListener('scroll', () => {
       reveals[i].classList.remove("show");
     }
   }
+
+  //make a sticky navbar by adding a class on scroll
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 });
+
+
